fd858fe34afb8d023a92ff25af580c5e9e851953
ichoyjx <ichoyjx1106@gmail.com>
2014-01-01 03:32:55 -0600

    2.1

--- 2.3.cpp
+++ 2.3.cpp
@@ -1,3 +1,8 @@
+/*
+ * 2.1, 2.3, 2.6
+ * Brian
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
@@ -5,7 +10,7 @@
 using namespace std;
 
 typedef char Type;
-#define INPUT "FOLLOW UP"
+#define INPUT "FOLLOW UP DUDE"
 
 struct LNode {
   Type elem;
@@ -35,20 +40,60 @@ void printList (LNode *L) {
   cout << endl;
 }
 
+LNode * deleteNext (LNode *L) {
+  if (L == NULL) { return L; }
+
+  LNode *deleted = L->next;
+  L->next = L->next->next;
+  //L->next->next = NULL;
+
+  delete deleted;
+  return L->next;
+}
+
+void deleteDups (LNode *L) {
+  // p, q will point to the first real node
+  LNode *p = L->next;
+  LNode *q = p;
+  LNode *pre = L; // point to the previous one
+
+  while (p) {
+    while (q) {
+      if ( (p!=q) && (q->elem==p->elem) ) {
+        cout << "deleting " << pre->next->elem << endl;
+        q = deleteNext(pre);   
+      } else {
+        pre = pre->next;
+        q = q->next;
+      }
+    }
+
+    pre = p;
+    p = p->next;
+    q = p;
+  }
+}
+
 int main() {
-  LNode *Head = NULL, *p = NULL;
+  LNode *Head = NULL, *p = NULL, *tmp;
   Head = (LNode *) malloc (sizeof(LNode));
   Head->elem = '$';
   Head->next = NULL;
   p = Head;
+  tmp = Head;
 
   string s = INPUT;
   int i;
   for (i=0; i<s.length(); i++) {
+    tmp = p;
     p = insertNode(p, s[i]);
     //cout << p->elem << endl;
   }
 
+  printList(Head);
+
+  // delete the duplicate nodes
+  deleteDups(Head);
   printList(Head);
 
   return 0;
