26938ac50a6518a1fb2ae40532cab536a6f53964
ichoyjx <ichoyjx1106@gmail.com>
2014-01-02 04:06:40 -0600

    stack and queue using C++

---
+++ 3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>       // std::cin, std::cout
+#include <queue>          // std::queue
+#include <stack>          // std::stack
+
+int main ()
+{
+  std::queue<int> myqueue;
+  int myint;
+
+  std::cout << "Please enter some integers (enter 0 to end):\n";
+
+  do {
+    std::cin >> myint;
+    myqueue.push (myint);
+  } while (myint);
+
+  std::cout << "myqueue contains: ";
+  while (!myqueue.empty())
+  {
+    std::cout << ' ' << myqueue.front();
+    myqueue.pop();
+  }
+  std::cout << '\n';
+
+  std::stack<int> mystack;
+
+  for (int i=0; i<5; ++i) mystack.push(i);
+
+  std::cout << "Popping out elements...";
+  while (!mystack.empty())
+  {
+     std::cout << ' ' << mystack.top();
+     mystack.pop();
+  }
+  std::cout << '\n';
+
+  return 0;
+}
